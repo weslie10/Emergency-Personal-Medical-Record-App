20445406c035f39547d1bbac7f47bcd275493ad2
weslie10 <weslie.leonardo10@gmail.com>
2021-05-24 01:00:20 +0700

    change startactivityforresult that have been deprecated  into activityresultcontract from androidX

--- app/src/main/java/com/capstone/personalmedicalrecord/ui/patient/data/records/RecordsFragment.kt
+++ app/src/main/java/com/capstone/personalmedicalrecord/ui/patient/data/records/RecordsFragment.kt
@@ -14,6 +14,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -27,7 +29,6 @@ import com.capstone.personalmedicalrecord.utils.DataDummy
 import com.capstone.personalmedicalrecord.utils.Utility.navigateTo
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import java.io.File
-
 
 class RecordsFragment : Fragment(), RecordsCallback {
 
@@ -86,7 +87,7 @@ class RecordsFragment : Fragment(), RecordsCallback {
 
     private fun takePhoto() {
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        photoFile = getPhotoFile(FILE_NAME)
+        photoFile = getPhotoFile()
         val providerFile = FileProvider.getUriForFile(
             requireContext(),
             "com.capstone.personalmedicalrecord.fileprovider",
@@ -95,7 +96,7 @@ class RecordsFragment : Fragment(), RecordsCallback {
         intent.putExtra(MediaStore.EXTRA_OUTPUT, providerFile)
 
         if (intent.resolveActivity(requireContext().packageManager) != null) {
-            startActivityForResult(intent, TAKE_PHOTO_CODE)
+            takePhoto.launch(intent)
         } else {
             Toast.makeText(context, "Camera could not open", Toast.LENGTH_SHORT).show()
         }
@@ -104,8 +105,7 @@ class RecordsFragment : Fragment(), RecordsCallback {
     private fun choosePhoto() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (requireContext().checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
-                val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-                requestPermissions(permissions, PERMISSION_CODE)
+                requestPermission.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
             } else {
                 chooseImageGallery()
             }
@@ -117,19 +117,19 @@ class RecordsFragment : Fragment(), RecordsCallback {
     private fun chooseImageGallery() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
-        startActivityForResult(intent, CHOOSE_PHOTO_CODE)
+        choosePhoto.launch(intent)
     }
 
     private fun chooseDocument() {
         var intent = Intent(Intent.ACTION_GET_CONTENT)
         intent.type = "*/*"
         intent = Intent.createChooser(intent, "Choose a file")
-        startActivityForResult(intent, CHOOSE_DOCUMENT_CODE)
+        chooseDocument.launch(intent)
     }
 
-    private fun getPhotoFile(fileName: String): File {
+    private fun getPhotoFile(): File {
         val storageDirectory = requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile(fileName, ".jpg", storageDirectory)
+        return File.createTempFile(FILE_NAME, ".jpg", storageDirectory)
     }
 
     override fun onRequestPermissionsResult(
@@ -148,24 +148,40 @@ class RecordsFragment : Fragment(), RecordsCallback {
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == TAKE_PHOTO_CODE) {
+    private val takePhoto =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
                 val takenImage = BitmapFactory.decodeFile(photoFile?.absolutePath)
 //            binding.imageView.setImageBitmap(takenImage)
-                Toast.makeText(context, "Image Already Taken", Toast.LENGTH_SHORT).show()
-            } else if (requestCode == CHOOSE_PHOTO_CODE) {
+                Toast.makeText(context, photoFile?.absolutePath, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    private val requestPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                chooseImageGallery()
+            } else {
+                Toast.makeText(context, "Permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    private val choosePhoto =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
 //            viewImage.setImageURI(data?.data)
                 Toast.makeText(context, "Image Choose", Toast.LENGTH_SHORT).show()
-            } else if (requestCode == CHOOSE_DOCUMENT_CODE) {
-                fileUri = data?.getData();
-                filePath = fileUri?.getPath();
+            }
+        }
+
+    private val chooseDocument =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                fileUri = result.data?.getData()
+                filePath = fileUri?.getPath()
                 Toast.makeText(context, filePath, Toast.LENGTH_SHORT).show()
             }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data)
         }
-    }
 
     override fun onItemClick(record: Record) {
         viewModel.setType("records")
@@ -179,9 +195,6 @@ class RecordsFragment : Fragment(), RecordsCallback {
 
     companion object {
         private const val FILE_NAME = "photo.jpg"
-        private const val TAKE_PHOTO_CODE = 1
-        private const val CHOOSE_PHOTO_CODE = 2
-        private const val CHOOSE_DOCUMENT_CODE = 3
         private const val PERMISSION_CODE = 1001
     }
 }
--- app/src/main/java/com/capstone/personalmedicalrecord/ui/patient/profile/UpdateProfileFragment.kt
+++ app/src/main/java/com/capstone/personalmedicalrecord/ui/patient/profile/UpdateProfileFragment.kt
@@ -1,12 +1,15 @@
 package com.capstone.personalmedicalrecord.ui.patient.profile
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.capstone.personalmedicalrecord.R
 import com.capstone.personalmedicalrecord.databinding.FragmentPatientUpdateProfileBinding
 import com.capstone.personalmedicalrecord.utils.Utility.clickBack
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class UpdateProfileFragment : Fragment() {
 
@@ -25,6 +28,25 @@ class UpdateProfileFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.saveChangesBtn.setOnClickListener {
             saveChanges()
+        }
+        binding.changePhoto.setOnClickListener {
+            val singleItems = arrayOf("Take a Photo", "Choose a photo")
+            var checkedItem = 0
+
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle(getString(R.string.add_record_text))
+                .setNeutralButton(getString(R.string.cancel), null)
+                .setPositiveButton(getString(R.string.ok)) { _, _ ->
+//                    when (checkedItem) {
+//                        0 -> takePhoto()
+//                        1 -> choosePhoto()
+//                    }
+                    Log.d("check",singleItems[checkedItem])
+                }
+                .setSingleChoiceItems(singleItems, 0) { _, which ->
+                    checkedItem = which
+                }
+                .show()
         }
         activity?.clickBack(binding.backBtn)
     }
