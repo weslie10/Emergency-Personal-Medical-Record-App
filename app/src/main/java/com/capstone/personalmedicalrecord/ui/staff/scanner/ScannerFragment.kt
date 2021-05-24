177a0610fa4ba936659925974c6b0a70ddd0a62c
weslie10 <weslie.leonardo10@gmail.com>
2021-05-24 15:33:15 +0700

    add scanner in staff features

--- app/src/main/java/com/capstone/personalmedicalrecord/ui/staff/scanner/ScannerFragment.kt
+++ app/src/main/java/com/capstone/personalmedicalrecord/ui/staff/scanner/ScannerFragment.kt
@@ -1,14 +1,15 @@
 package com.capstone.personalmedicalrecord.ui.staff.scanner
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.capstone.personalmedicalrecord.databinding.FragmentStaffScannerBinding
+import com.google.zxing.integration.android.IntentIntegrator
 
 class ScannerFragment : Fragment() {
 
@@ -35,6 +36,37 @@ class ScannerFragment : Fragment() {
 //            textView.text = it
 //        })
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.scanBtn.setOnClickListener {
+            setUpScanner()
+        }
+    }
+
+    private fun setUpScanner() {
+        IntentIntegrator(activity).apply {
+            setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+            setCameraId(0)
+            setOrientationLocked(false)
+            setPrompt("scanning")
+            setBeepEnabled(true)
+            setBarcodeImageEnabled(true)
+            initiateScan()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result.contents != null) {
+//            ClipboardManager manager =(ClipboardManager) getSystemService (CLIPBOARD_SERVICE);
+//            ClipData data = ClipData.newPlainText(“result”, result.getContents());
+//            manager.setPrimaryClip(data)
+            Toast.makeText(context, result.contents, Toast.LENGTH_LONG).show()
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     override fun onDestroyView() {
